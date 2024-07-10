dab7228d9ebc2df68c69091cc5d5b034aa69e460
ShuraifShoaib <shuraifshoaib@gmail.com>
2024-07-10 20:18:49 +0500

    Javascript assign5

---
+++ script.js
@@ -0,0 +1,44 @@
+const user = prompt("what table you want 1,2,3,4,5,6,7,8,9,10")
+if (user == 1)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`1 x ${i} = ${1 * i}`);
+   }
+if (user == 2)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`2 x ${i} = ${2 * i}`);
+   }
+if (user == 3)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`3 x ${i} = ${3 * i}`);
+   }
+if (user == 4)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`4 x ${i} = ${4 * i}`);
+   }
+if (user == 5)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`5 x ${i} = ${5 * i}`);
+   }
+if (user == 6)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`6 x ${i} = ${6 * i}`);
+   }
+if (user == 7)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`7 x ${i} = ${7 * i}`);
+   }
+if (user == 8)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`8 x ${i} = ${8 * i}`);
+   }
+if (user == 9)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`9 x ${i} = ${9 * i}`);
+   }
+if (user == 10)
+   for (let i = 1; i <= 10; i++) {
+      console.log(`10 x ${i} = ${10 * i}`);
+   }
+
+
+
